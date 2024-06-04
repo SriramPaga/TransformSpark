893858b59fca216527b644924e8a6245d3fb6797
Sriram Paga <pagasriram@gmail.com>
2024-06-04 19:43:00 +0530

    hashrouter added

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SparkView from './views/SparkView';
 import NavigationBar from './Components/NavigationBar';
 import LoadingPage from './Components/LoadingPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+// change hasRouter before hosting on main domain, hashrouter being used for Github pages
 // color refernce
 // https://coolors.co/f42845-f37a3e-eea53c-f18f3f-ebc038
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<LoadingPage />} />
           <Route path="/Home" element={<LoadingPage />} />
           <Route path="/Login" element={<LoadingPage />} />
           <Route path="/Spark" element={<SparkView />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
